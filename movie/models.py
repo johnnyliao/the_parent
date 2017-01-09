dcc63524f070706266028ee48b3220dd5610520b
johnnyliao <epyonss@gmail.com>
2017-01-09 18:07:12 +0800

    增加影片介紹數字

--- movie/models.py
+++ movie/models.py
@@ -29,7 +29,7 @@ TYPE_CHOICES = (
 class Movie(models.Model):
     link = models.CharField(_(u"youtube連結"), max_length=100)
     title = models.CharField(_(u"標題"), max_length=100)
-    description = models.CharField(_(u"簡介"), max_length=100)
+    description = models.CharField(_(u"簡介"), max_length=2000)
     movie_type = models.CharField(_(u"分類"), max_length=100, choices=TYPE_CHOICES)
 
     class Meta:
