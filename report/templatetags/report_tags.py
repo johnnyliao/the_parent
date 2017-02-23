22835f0ca70a790719ba2eb6cdca4c3274bad758
johnnyliao <epyonss@gmail.com>
2017-02-23 14:49:10 +0800

    bug fix

--- report/templatetags/report_tags.py
+++ report/templatetags/report_tags.py
@@ -9,16 +9,22 @@ def running_total(list_total):
 
 @register.filter
 def week_report(list_data, start):
-	today = datetime.date.today()
-	list_data = list_data.filter(date__gte=start,date__lte=today).order_by("-date")[::-1]
-	start_date = list_data[0]
-	last_date = list_data[len(list_data) - 1]
-	return last_date.hit_count - start_date.hit_count
+	if list_data.count():
+		today = datetime.date.today()
+		list_data = list_data.filter(date__gte=start,date__lte=today).order_by("-date")[::-1]
+		start_date = list_data[0]
+		last_date = list_data[len(list_data) - 1]
+		return last_date.hit_count - start_date.hit_count
+	else:
+		return ""
 
 @register.filter
 def month_report(list_data, start):
-	today = datetime.date.today()
-	list_data = list_data.filter(date__gte=start,date__lte=today).order_by("-date")[::-1]
-	start_date = list_data[0]
-	last_date = list_data[len(list_data) - 1]
-	return last_date.hit_count - start_date.hit_count
+	if list_data.count():
+		today = datetime.date.today()
+		list_data = list_data.filter(date__gte=start,date__lte=today).order_by("-date")[::-1]
+		start_date = list_data[0]
+		last_date = list_data[len(list_data) - 1]
+		return last_date.hit_count - start_date.hit_count
+	else:
+		return ""
