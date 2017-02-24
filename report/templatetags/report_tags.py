76fb64e6846977f9d565d39c1f85ee6554c77822
johnnyliao <epyonss@gmail.com>
2017-02-24 10:01:40 +0800

    bug fiz

--- report/templatetags/report_tags.py
+++ report/templatetags/report_tags.py
@@ -9,22 +9,28 @@ def running_total(list_total):
 
 @register.filter
 def week_report(list_data, start):
-	if list_data.count():
+	if list_data.every_day_data.all().count():
 		today = datetime.date.today()
-		list_data = list_data.filter(date__gte=start,date__lte=today).order_by("-date")[::-1]
-		start_date = list_data[0]
+		list_data = list_data.every_day_data.all().filter(date__gte=start,date__lte=today).order_by("-date")[::-1]
+		if list_data.date >= start and list_data.date <= today:
+			start_count = list_data.base_count
+		else:
+			start_count = list_data[0].hit_count
 		last_date = list_data[len(list_data) - 1]
-		return last_date.hit_count - start_date.hit_count
+		return last_date.hit_count - start_count
 	else:
 		return ""
 
 @register.filter
 def month_report(list_data, start):
-	if list_data.count():
+	if list_data.every_day_data.all().count():
 		today = datetime.date.today()
-		list_data = list_data.filter(date__gte=start,date__lte=today).order_by("-date")[::-1]
-		start_date = list_data[0]
+		list_data = list_data.every_day_data.all().filter(date__gte=start,date__lte=today).order_by("-date")[::-1]
+		if list_data.date >= start and list_data.date <= today:
+			start_count = list_data.base_count
+		else:
+			start_count = list_data[0].hit_count
 		last_date = list_data[len(list_data) - 1]
-		return last_date.hit_count - start_date.hit_count
+		return last_date.hit_count - start_count
 	else:
 		return ""
