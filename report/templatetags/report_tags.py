e323747d3b71f0260f8bd0a50a51a73cbd07f1a8
johnnyliao <epyonss@gmail.com>
2017-02-24 10:09:51 +0800

    bug fix

--- report/templatetags/report_tags.py
+++ report/templatetags/report_tags.py
@@ -11,12 +11,12 @@ def running_total(list_total):
 def week_report(list_data, start):
 	if list_data.every_day_data.all().count():
 		today = datetime.date.today()
-		list_data = list_data.every_day_data.all().filter(date__gte=start,date__lte=today).order_by("-date")[::-1]
-		if list_data.date >= start and list_data.date <= today:
+		list_datas = list_data.every_day_data.all().filter(date__gte=start,date__lte=today).order_by("-date")[::-1]
+		if list_data.date.date() >= start and list_data.date.date() <= today:
 			start_count = list_data.base_count
 		else:
-			start_count = list_data[0].hit_count
-		last_date = list_data[len(list_data) - 1]
+			start_count = list_datas[0].hit_count
+		last_date = list_datas[len(list_datas) - 1]
 		return last_date.hit_count - start_count
 	else:
 		return ""
@@ -25,12 +25,14 @@ def week_report(list_data, start):
 def month_report(list_data, start):
 	if list_data.every_day_data.all().count():
 		today = datetime.date.today()
-		list_data = list_data.every_day_data.all().filter(date__gte=start,date__lte=today).order_by("-date")[::-1]
-		if list_data.date >= start and list_data.date <= today:
+		list_datas = list_data.every_day_data.all().filter(date__gte=start,date__lte=today).order_by("-date")[::-1]
+
+		#import pdb;pdb.set_trace()
+		if list_data.date.date() >= start and list_data.date.date() <= today:
 			start_count = list_data.base_count
 		else:
-			start_count = list_data[0].hit_count
-		last_date = list_data[len(list_data) - 1]
+			start_count = list_datas[0].hit_count
+		last_date = list_datas[len(list_datas) - 1]
 		return last_date.hit_count - start_count
 	else:
 		return ""
