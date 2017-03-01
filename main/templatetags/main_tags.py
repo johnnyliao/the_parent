5f1766aaf1f1b98eb6a976369da7a5acdbb27094
johnnyliao <epyonss@gmail.com>
2017-03-01 15:48:14 +0800

    增加連結

--- main/templatetags/main_tags.py
+++ main/templatetags/main_tags.py
@@ -8,8 +8,5 @@ def get_name(in_data):
 	return in_data.extra_data["name"]
 
 @register.filter
-def get_img(in_data):
-	import pdb;pdb.set_trace()
-	fb_id = in_data.extra_data["id"]
-	pic_url = "http://graph.facebook.com/v2.8/%s/picture" % fb_id
-	return pic_url
+def get_fb_link(in_data):
+	return "https://www.facebook.com/" + in_data.extra_data["id"]
